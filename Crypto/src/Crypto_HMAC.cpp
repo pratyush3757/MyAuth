665acf883db7a448c6b5ece814cbeb6d45cd4e40
Pro3757 <pratyush3757@gmail.com>
2021-10-24 01:34:46 +0530

    HMAC Refactored to single small function

--- Crypto/src/Crypto_HMAC.cpp
+++ Crypto/src/Crypto_HMAC.cpp
@@ -4,25 +4,49 @@
 #include <cryptopp/hmac.h>
 #include <cryptopp/sha.h>
 #include <cryptopp/filters.h>
+#include <cryptopp/channels.h>
 #include <cryptopp/hex.h>
 #define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
 #include <cryptopp/md5.h>
 
 #include "Crypto_HMAC.h"
 
-static std::string getHmacMd5(const std::string& hmacSecretKey, const std::string& hexEncodedMessage){
+std::string getHmacForGivenAlgorithm(const std::string& hmacSecretKey, const std::string& hexEncodedMessage,const std::string& algorithm){
     std::string mac;
     
     mac.clear();
     try
     {
+        //HMAC Transforms
+        CryptoPP::HMAC <CryptoPP::Weak::MD5> hmacMd5((byte *)hmacSecretKey.c_str(), hmacSecretKey.size());
+        CryptoPP::HMAC <CryptoPP::SHA1> hmacSha1((byte *)hmacSecretKey.c_str(), hmacSecretKey.size());
+        CryptoPP::HMAC <CryptoPP::SHA256> hmacSha256((byte *)hmacSecretKey.c_str(), hmacSecretKey.size());
+        CryptoPP::HMAC <CryptoPP::SHA512> hmacSha512((byte *)hmacSecretKey.c_str(), hmacSecretKey.size());
         
-        CryptoPP::HMAC <CryptoPP::Weak::MD5> hmac((byte *)hmacSecretKey.c_str(), hmacSecretKey.size());
+        //Hash Filters
+        CryptoPP::HashFilter hashFilterMd5(hmacMd5,new CryptoPP::StringSink(mac));
+        CryptoPP::HashFilter hashFilterSha1(hmacSha1,new CryptoPP::StringSink(mac));
+        CryptoPP::HashFilter hashFilterSha256(hmacSha256,new CryptoPP::StringSink(mac));
+        CryptoPP::HashFilter hashFilterSha512(hmacSha512,new CryptoPP::StringSink(mac));
+        
+        CryptoPP::ChannelSwitch cs;
+        
+        if(algorithm=="MD5"){
+            cs.AddDefaultRoute(hashFilterMd5);
+        }
+        else if(algorithm=="SHA1"){
+            cs.AddDefaultRoute(hashFilterSha1);
+        }
+        else if(algorithm=="SHA256"){
+            cs.AddDefaultRoute(hashFilterSha256);
+        }
+        else if(algorithm=="SHA512"){
+            cs.AddDefaultRoute(hashFilterSha512);
+        }
+        
         CryptoPP::StringSource(hexEncodedMessage, true,
                 new CryptoPP::HexDecoder(
-                    new CryptoPP::HashFilter(hmac,
-                        new CryptoPP::StringSink(mac)
-                    )
+                    new CryptoPP::Redirector(cs)
                 )
         );
     }
@@ -30,82 +54,4 @@ static std::string getHmacMd5(const std::string& hmacSecretKey, const std::strin
         std::cerr << e.what() << std::endl;
     }
     return mac;
-}
-
-static std::string getHmacSha1(const std::string& hmacSecretKey, const std::string& hexEncodedMessage){
-    std::string mac;
-
-    mac.clear();
-    try
-    {
-        
-        CryptoPP::HMAC <CryptoPP::SHA1> hmac((byte *)hmacSecretKey.c_str(), hmacSecretKey.size());
-        CryptoPP::StringSource(hexEncodedMessage, true,
-                new CryptoPP::HexDecoder(
-                    new CryptoPP::HashFilter(hmac,
-                        new CryptoPP::StringSink(mac)
-                    )
-                )
-        );
-    }
-    catch(const CryptoPP::Exception& e){
-        std::cerr << e.what() << std::endl;
-    }
-    return mac;
-}
-
-static std::string getHmacSha256(const std::string& hmacSecretKey, const std::string& hexEncodedMessage){
-    std::string mac;
-
-    mac.clear();
-    try
-    {
-        
-        CryptoPP::HMAC <CryptoPP::SHA256> hmac((byte *)hmacSecretKey.c_str(), hmacSecretKey.size());
-        CryptoPP::StringSource(hexEncodedMessage, true,
-                new CryptoPP::HexDecoder(
-                    new CryptoPP::HashFilter(hmac,
-                        new CryptoPP::StringSink(mac)
-                    )
-                )
-        );
-    }
-    catch(const CryptoPP::Exception& e){
-        std::cerr << e.what() << std::endl;
-    }
-    return mac;
-}
-
-static std::string getHmacSha512(const std::string& hmacSecretKey, const std::string& hexEncodedMessage){
-    std::string mac;
-
-    mac.clear();
-    try
-    {
-        
-        CryptoPP::HMAC <CryptoPP::SHA512> hmac((byte *)hmacSecretKey.c_str(), hmacSecretKey.size());
-        CryptoPP::StringSource(hexEncodedMessage, true,
-                new CryptoPP::HexDecoder(
-                    new CryptoPP::HashFilter(hmac,
-                        new CryptoPP::StringSink(mac)
-                    )
-                )
-        );
-    }
-    catch(const CryptoPP::Exception& e){
-        std::cerr << e.what() << std::endl;
-    }
-    return mac;
-}
-
-std::string getHmacForGivenAlgorithm(const std::string& hmacSecretKey, const std::string& hexEncodedMessage,const std::string& algorithm){
-    if(algorithm == "MD5")
-        return getHmacMd5(hmacSecretKey,hexEncodedMessage);
-    if(algorithm == "SHA1")
-        return getHmacSha1(hmacSecretKey,hexEncodedMessage);
-    if(algorithm == "SHA256")
-        return getHmacSha256(hmacSecretKey,hexEncodedMessage);
-    if(algorithm == "SHA512")
-        return getHmacSha512(hmacSecretKey,hexEncodedMessage);
-    return "";
 }
